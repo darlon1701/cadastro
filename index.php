6872eeff49962836c5a9983f87cf976e2b332dc8
Darlon Melo <darlon.melo@bap.net>
2021-06-02 16:55:10 -0300

    02.06.2021

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+    <title>Cadastro</title>
+
+</head>
+<body>
+    <div class="container">
+        
+        <div class="caixa1">
+    
+        </div>
+
+        <div class="caixa2">
+            <div class="caixote">                
+                <form action="">
+                    <label>
+                        Nome: &nbsp; <input type="text" id="nome" name="nome" placeholder="Digite seu nome completo"></input> <br>
+                        E-mail: &nbsp;<input type="email" id="email" name="email" placeholder="Digite seu email"></input> <br>
+                        Tel:&nbsp; <input type="text" id="telefone" name="telefone" placeholder="Digite seu telefone"></input> <br>
+                        Idade: &nbsp; <input type="number" id="idade" name="idade" placeholder="Digite sua idade"></input> <br>                        
+                    </label>
+
+                    <button>CADASTRAR</button>
+
+                </form>
+            </div>
+        </div>
+    
+    </div>  
+</body>
+</html>
